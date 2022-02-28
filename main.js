f2db8bc51d34c2582fd8ab5973c10fb8d0c60051
Jiya15-102011 <98893406+Jiya15-102011@users.noreply.github.com>
2022-02-28 15:35:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function c(){
+    x=Number(document.getElementById("a").value);
+    p=Number(document.getElementById("b").value);
+j=x+p;
+console.log("the addition is",j);
+t=x-p;
+console.log("the substaction is",t);
+v=x*p;
+console.log("the multiplication is",v);
+r=x/p;
+console.log("the division is",r);
+}
